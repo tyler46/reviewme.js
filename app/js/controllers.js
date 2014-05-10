4f82516d76221ae337bffb89b0387a8dd96ef53c
tyler46 <mail.doctor46@gmail.com>
2014-05-10 18:52:40 +0300

    Added dropdown select for repos language.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,12 +2,14 @@
 
 /* Controllers */
 
-var reviewmeControllers = angular.module('reviewmeControllers', []);
+var reviewmeControllers = angular.module('reviewmeControllers', ['ui.utils']);
 
 reviewmeControllers.controller('RepositoryListCtrl', ['$scope', 'Repositories',
-  function($scope, Repository) {
-    $scope.repositories = Repository.query();
+  function($scope, Repositories) {
+    $scope.repositories = Repositories.query();
     $scope.orderProp = 'updated_at';
+    $scope.language = 'language';
+
   }]);
 
 reviewmeControllers.controller('RepositoryDetailCtrl', ['$scope', '$routeParams', 'Repository',
